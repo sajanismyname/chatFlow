dd83615b3eb8cb930bacb73a2e1a56fd6b69abbb
Sajan Limbu <limbusajan087@gmail.com>
2026-09-17 15:07:34 +0545

    feat(server): initialize server with TypeScript, Express, and essential entities
    - Added package.json for server dependencies and scripts
    - Created initial server setup in server.ts with Express and CORS
    - Defined TypeScript configuration in tsconfig.json
    - Added entity files for Conversation, ConversationMember, Message, and User

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import express from "express"
+import cors from "cors"
+
+const app=express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res)=>{
+    res.json({
+        message:"ChatFlow api is running"
+    })
+})
+
+const PORT=5000;
+
+app.listen(PORT,()=>{
+    console.log(`Server running on http://localhost:${PORT}`)
+})
